3bde8eadd3565aa59c0460bc7b23feb1e9c811eb
James <jamesgeeling@hotmail.co.uk>
2024-01-28 03:51:06 +0000

    Scraper added, and excel file

--- scraper.py
+++ scraper.py
@@ -1,70 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import time
 
 
 html_text = requests.get(
-    "https://wearecodenation.com/2024/01/23/data-course-playground/"
+    "https://funkymonkie.github.io/website-project/"
 ).text
 
 souped_html = BeautifulSoup(html_text, "lxml")
 
-h5s = souped_html.find_all(
-    "h5", class_="elementor-heading-title elementor-size-default"
-)
+# Extract data from the My Top 3 Favourite section
+categories = souped_html.select(".sections .category")
 
-h6s = souped_html.find_all("h6")
-# print(h5s)
+data = {"Category": [], "First Favourite": [], "Second Favourite": [], "Third Favourite": []}
 
-# for h5 in h5s:
-#     if ":" in h5.text:
-#         print(h5.text)
-#         h6_match = h5.find_next('h6')
-#         for single_date in h6_match.strings:
-#             print(single_date)
+for category in categories:
+    category_name = category.select_one(".title h3").text.strip()
+    items = [item.text.strip() for item in category.find_all("dd")[:3]]
 
+    data["Category"].append(category_name)
+    data["First Favourite"].append(items[0] if len(items) > 0 else "")
+    data["Second Favourite"].append(items[1] if len(items) > 1 else "")
+    data["Third Favourite"].append(items[2] if len(items) > 2 else "")
+    
+    # Delay in requests to not get banned from site
+    time.sleep(2)
+    
 
-# months = [
-#         'January','February','March','April','May','June','July',
-#         'August','September','October','November','December']
+# Create a Pandas DataFrame from the extracted data
+df = pd.DataFrame(data)
 
-
-# for h6 in h6s:
-#     if any(month in h6.text for month in months):
-#         print(h6.text)
-#         course_dates.append(h6.text)
-
-
-# for h6 in h6s:
-
-# create dict
-course_dates_dict = {}
-
-# add values to dict
-for h5 in h5s:
-    if ":" in h5.text:
-        key = h5.text
-        values = []
-
-        h6_match = h5.find_next("h6")
-        for single_date in h6_match.stripped_strings:
-            values.append(single_date)
-
-        course_dates_dict[key] = values
-
-# for key, values in course_dates_dict.items():
-#     print(f"{key}: {values}")
-
-
-# Create Dataframe from dict
-# df = pd.DataFrame(list(course_dates_dict.items()), columns=["COURSE", "DATES"])
-
-# df = pd.DataFrame({key:pd.Series(value) for key value in courses.items()})
-
-df = pd.DataFrame.from_dict(course_dates_dict, orient="index").T
+#df = pd.DataFrame.from_dict(data, orient="index").T
 print(df)
 
 # create excel
-excel_filename = "scraped_cn_data.xlsx"
+excel_filename = "scraped_Fav_3_data.xlsx"
 df.to_excel(excel_filename, index=False)
 print(f"Scraped data has been saved to {excel_filename}.")
